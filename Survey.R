2c35f92b7b5f0c01d95852317985dbb4e4940202
sflynn2 <sflynn2@patriots.uttyler.edu>
2015-11-27 09:23:17 -0600

    Final Commit-Really

--- Survey.R
+++ Survey.R
@@ -1,41 +1,41 @@
 ## Survey Project in R
 
-#install and load libraries
+#install and load "dplyr" and "ggplot2" libraries
 install.packages("dplyr")
 install.packages ("ggplot2")
 library(dplyr)
 library(ggplot2)
 
-# download data file
+# download survey data file from website
 download.file("http://files.figshare.com/2236372/combined.csv",  "data/portal_data_joined.csv")# data for project is in the portal_data_joined.csv
 
-# import data file
+# import survey data file
 surveys <- read.csv('data/portal_data_joined.csv')
 
 ## explore relationship between weight and hindfoot length for each species
 
 #data parsing
 
-# Selects the species id, hindfoot length and weight data from surveys, removes NA from weight and hindfoot length. Puts it all in the file survey sml
+# Select the species id, hindfoot length and weight data from datafile "surveys", removes NA from weight and hindfoot length. Puts it all in the new object " survey_sml
 surveys_sml<- surveys %>%  
   select(species_id, hindfoot_length, weight)%>%
   filter(!is.na(weight))%>%  
   filter(!is.na(hindfoot_length))
 
-#Find the mean for weight and group by species_id
+#Find the mean for weight and group by species_id from object surveys_sml and put it into a new object called weight_mean
 weight_mean<-surveys_sml%>%
   group_by(species_id)%>%
   summarize(mean_weight = mean(weight, na.rm = TRUE))
 
-# Find the mean for hindfoot length  and group by species _id
+# Find the mean for hindfoot length  and group by species _id from object surveys_sml and put it into new object called hindfoot_length_mean
 hindfoot_length_mean<-surveys_sml%>%
 group_by(species_id)%>%
   summarize(mean_hindfoot_length = mean(hindfoot_length, na.rm = TRUE))
 
-# combine data from mean_weight and mean_hindfoot_length
+# combine data from mean_weight and mean_hindfoot_length into one object called Combined_means
 Combined_means<-cbind(weight_mean, hindfoot_length_mean)
   
-#build figure
+#build figure to show relationship between average weight and hindfoot length
 
 #Scatter plot with modified color, size of points in the point layer,  modify x-axis and y-axis labels and add a title
 ggplot(data = Combined_means, aes(x=mean_weight, y=mean_hindfoot_length, color = species_id)) + geom_point(size = 2.5) + xlab("Average Weight - g") + ylab("Average Hindfoot Length - mm") + ggtitle("Figure 1")
@@ -51,14 +51,14 @@ fit<- aov(weight ~ hindfoot_length, data=surveys_sml) # fit model
 fit # look at fit
 summary(fit) #summarize and show results
 
-## Comparison of average weight by year (line graph)by species
+## Comparison of average weight by year by species
 
-# select year and weight data only and filter out the NA data
+# select year and weight data only from "surveys" data, and filter out the NA data from weight and save it into a new object "survey_com
 survey_com<- surveys %>%  
   select(weight, year)%>%
   filter(!is.na(weight))
 
-# Create a new object "weight_mean from survey_com.  Group data by year
+# Create a new object "weight_mean from survey_com.  Group data by year and find mean for the weight.  Remove all NA from data.
 weight_mean<-survey_com%>%
   group_by(year)%>%
   summarize(mean_weight = mean(weight, na.rm = TRUE))
@@ -71,14 +71,14 @@ ggsave("Figure 2.pdf")
 
 ## Distribution of weight in males
 
-# select only data concerned with weight and males of organisms, remove all NA in the weight and species id data; create new object survey_dis
+# select only data concerned with weight and males of organisms, remove all NA in the weight and species id data; store data in a new object survey_dis
 survey_dis<-surveys%>%
   filter(sex == 'M')%>%
   select(sex, weight, species_id)%>%
    filter(!is.na(weight))%>%
   filter(!is.na(species_id))
 
-# create histogram of weight of males
+# create histogram of weight of males; choosing color and fill; labeling x-aXIS and y-axis and titling the figure
 
 ggplot(survey_dis, aes(x=weight)) +
   geom_histogram(binwidth= 1.0, colour="black", fill="green") + xlab("weight-g") + ylab("Number of Males") + ggtitle("Figure 3")
@@ -86,7 +86,7 @@ ggplot(survey_dis, aes(x=weight)) +
 # export to pdf
 ggsave("Figure 3.pdf")
 
-# filter weight to between 0-100 to better visualize the majority of the data. Filter out the NA from weight and species ID and keep only the Male data
+# filter weight to between 0-100 to better visualize the majority of the data. Filter out the NA from weight and species ID and keep only the Male data. Store data in a new object called survey_dis_weight_limited
 survey_dis_weight_limited<-surveys%>%
   filter(sex == 'M')%>%
   select(sex, weight, species_id)%>%
@@ -101,14 +101,14 @@ ggplot(survey_dis_weight_limited, aes(x=weight)) +
 # export as pdf
 ggsave("Figure 4.pdf")
 
-# create new object with no filter for weight,selection for male, no selection for hindfootlength; filter out NA from weight and species ID
+# create new object called survey_species containing data from "surveys" with data for weight,selection for male and species ID, filter out NA from weight and species ID; 
 survey_species<-surveys%>%
   filter(sex == 'M')%>%
   select(sex, weight, species_id)%>%
   filter(!is.na(weight))%>%
   filter(!is.na(species_id))
 
-# create histogram of weight of male per species
+# create histogram of weight of male per species using a facet wrap
 ggplot(survey_species, aes(x=weight)) +
   geom_histogram(binwidth= 1.0, colour="green") + xlab("weight=g") + ylab("Number of Males") + ggtitle("Figure 5") + facet_wrap(~ species_id)
 
